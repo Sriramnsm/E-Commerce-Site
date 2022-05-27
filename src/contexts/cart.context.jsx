02cbdca01d5f37f65adb4bd233a9251ce9425179
Sriram Nsm <sriramnsmoorthy@gmail.com>
2022-05-27 13:20:51 +0530

    Implemented adding products to cart dropdown using cart context

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,17 +1,40 @@
 import { createContext, useState } from 'react';
-// eslint-disable-next-line
-import CartDropdown from "../components/cart-dropdown/cart-dropdown.component";
+
+export const addCartItem = (cartItems, productToAdd) => {
+    // Find if cartItems contains the productToAdd
+    const existingCartItem = cartItems.find(
+        (cartItem) => cartItem.id === productToAdd.id
+    );
+    // If found, Increment the quantity
+
+    if(existingCartItem){
+        return cartItems.map((cartItem) => cartItem.id===productToAdd.id ? 
+            {...cartItem, quantity: cartItem.quantity+1}
+            : cartItem
+        );
+    }
+    // return new array with modified cartItems/ new cart item
+    return [...cartItems, {...productToAdd, quantity:1}];
+}
 
 export const CartContext = createContext({
     isCartOpen: false,
-    setIsCartOpen: () => {},
-})
+    setIsOpen: () => {},
+    cartItems: [],
+    addItemToCart: () => {},
+});
 
 export const CartProvider = ({children}) => {
 
-    const [isCartOpen, setIsCartOpen] = useState(false)
-    const value = {isCartOpen,setIsCartOpen};
+    const [isCartOpen, setIsCartOpen] = useState(false);
+    const [cartItems, setCartItems] = useState([]);
+
+    const addItemToCart = (productToAdd) => {
+        setCartItems(addCartItem(cartItems, productToAdd));
+    }
+
+    const value = {isCartOpen,setIsCartOpen, cartItems, addItemToCart};
     return (
         <CartContext.Provider value={value}>{children}</CartContext.Provider>
-    )
-}
+    );
+};
